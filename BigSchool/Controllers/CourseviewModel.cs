ff6566436a1fb0fc45862636e3803f14bc68058f
Administrator <Administrator@PC208>
2020-06-01 16:16:01 +0700

    Add project files.

---
+++ BigSchool/Controllers/CourseviewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using BigSchool.Models;
+
+namespace BigSchool.Controllers
+{
+    internal class CourseviewModel
+    {
+        public List<Category> Categories { get; set; }
+    }
+}
